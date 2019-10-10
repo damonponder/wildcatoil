688eabc89490c0656884fe3ba1369e1f34894851
dgr8ninja <dr4602@gmail.com>
2019-10-10 09:06:36 -0500

    This is the first commit for 101019

--- client/app.js
+++ client/app.js
@@ -12,3 +12,23 @@ app.get("/", function(req, res) {
   });
 
   app.listen(port, () => console.log(`App running on port ${port}`));
+
+
+  app.get("/about/us", function(req, res) {
+    res.render("about", { title: "Hey", message: "Hello there!" });
+  });
+  
+  app.get("/dashboard", function(req, res) {
+    res.render("dashboard");
+  });
+  
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+  
+  app.post("/login", function(req, res) {
+    console.log(req.body);
+    // check if user's email and password are valid
+    // save login state for browser
+    res.redirect("/dashboard");
+  });
